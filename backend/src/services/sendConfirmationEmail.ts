8637257186486f12d9bd885acb3eee85de2067d8
Djamel Nait Sidenas <xamel.ns@gmail.com>
2025-04-18 13:24:09 -0400

    add email confirmation

---
+++ backend/src/services/sendConfirmationEmail.ts
@@ -0,0 +1,22 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: 'localhost',
+    port: 1025,
+    ignoreTLS: true
+});
+
+console.log(process.env.FRONTEND_URL);
+const sendConfirmationEmail = async (email: string, token: string) => {
+    const confirmUrl = `${process.env.FRONTEND_URL}/confirm-email/${token}`;
+    const mailOptions = {
+        from: "no-reply@retromarket.com",
+        to: email,
+        subject: 'Confirmez votre inscription',
+        html: `<p>Bienvenue ! Cliquez sur ce lien pour confirmer votre compte :</p>
+           <a href="${confirmUrl}">${confirmUrl}</a>`,
+    };
+
+    await transporter.sendMail(mailOptions);
+};
+export default sendConfirmationEmail;
